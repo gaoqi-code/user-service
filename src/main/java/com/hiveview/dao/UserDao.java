3907aa827bbdad3896a98b878f755ebc6d74f061
zhangsongwei <zhangsongwei@99114.com>
2017-05-17 20:56:02 +0800

    生产代码提交

--- src/main/java/com/hiveview/dao/UserDao.java
+++ src/main/java/com/hiveview/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.hiveview.dao;
 
 import com.hiveview.entity.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -12,20 +13,20 @@ public interface UserDao extends IBaseDao<User> {
 	public int addUser(User user);
 		//return this.addInfo("UserDao.addUser",user);
 
-	public User getUserByUnionid(String unionid);
+	public User getUserByUnionid(@Param("unionid")String unionid);
 		//return this.getInfoById("UserDao.getUserByUnionid",unionid);
 
-	public User getUserByParentId(int parentId);
+	public User getUserByParentId(@Param("parentId")int parentId);
 		//return this.getInfoById("UserDao.getUserByParentId",parentId);
 
-	public int updateUserJianBalance(int userId,BigDecimal totalFee);
+	public int updateUserJianBalance(@Param("userId")int userId, @Param("totalFee")BigDecimal totalFee);
 //		Map<String,Object> map = new HashMap<String,Object>();
 //		map.put("id",userId);
 //		map.put("totalFee",totalFee);
 //		return this.updateByIf("UserDao.updateUserJianBalance",map);
 //	}
 
-	public int updateUserJiaBalance(int userId,BigDecimal totalFee);
+	public int updateUserJiaBalance(@Param("userId")int userId, @Param("totalFee")BigDecimal totalFee);
 //		Map<String,Object> map = new HashMap<String,Object>();
 //		map.put("id",userId);
 //		map.put("totalFee",totalFee);
