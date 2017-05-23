da7e0dda9c6c5ef9ac94cb26afd7931db4a56dc3
zhangsongwei <zhangsongwei@99114.com>
2017-05-23 20:02:52 +0800

    生产代码提交

--- src/main/java/com/hiveview/contants/ContantType.java
+++ src/main/java/com/hiveview/contants/ContantType.java
@@ -4,12 +4,14 @@ import java.io.File;
 
 public class ContantType {
 
-	//1:充值，2:金蛋消费，3:砸蛋，4.佣金 5.微盘消费 6.微盘盈收
+	//1:充值，2:金蛋消费，3:砸蛋，4.佣金 5.微盘消费 6.微盘盈收 7.提现
 	public static int balanceLogType_1 = 1;
 	public static int balanceLogType_2 = 2;
 	public static int balanceLogType_3 = 3;
 	public static int balanceLogType_4 = 4;
 	public static int balanceLogType_5 = 5;
 	public static int balanceLogType_6 = 6;
+	public static int balanceLogType_7 = 7;
+
 
 }
